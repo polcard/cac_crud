f94306f87cd10f11ac15171c966e7512dfc2efff
polcard <polcard.dev@gmail.com>
2023-01-15 16:52:01 -0300

    arreglo de login para subida previa a presentacion

--- login.php
+++ login.php
@@ -4,15 +4,6 @@
 require_once('conexion.php');
     #validar datos
     if ($_POST){
-      #conexion a la base
-      #mail
-      #contraseña
-      #es_admin s o n 
-      /*
-      select mail, pass
-      from usuarios where
-      es_admin = 'S';*/
-      /* USUARIOS["mail"] */
         
         $user = $_POST["user"];
         $pass = $_POST["pass"];
@@ -51,158 +42,37 @@ require_once('conexion.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario</title>
 
-   <style>
-       @import url('https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Dongle&family=Fjalla+One&family=Lato:ital@1&family=Montserrat:wght@300&family=Palette+Mosaic&family=Poppins:ital,wght@0,400;0,600;0,700;1,700&family=Roboto:wght@300&family=The+Nautigal:wght@700&display=swap');
-        *{
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            font-family: Montserrat, sans-serif;
-        }
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="css/estilos.css" rel="stylesheet">
 
-        body{
-            height: 100vh;
-            width: 100%;
-        }
-        .container{
-            position:relative;
-            width: 100%;
-            height: 100%;
-            display:flex;   
-            justify-content: center;
-            align-items: center;
-            padding: 20px 100px;
-        }
-        .container::after{
-            content: '';
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            top: 0;
-            left: 0;
-            background-image: url("img/crud.jpg");
-            background-repeat: no-repeat;
-            background-position: center;
-            filter:blur(50px);
-            z-index: -1;
-           
-            background-color: rgba(0,0,0,0.5);
-        }
-        .contact-box{
-            max-width: 850px;
-            display:grid;
-            grid-template-columns: 1fr 1fr;
-            justify-content: center;
-            align-items: center;
-            text-align: center;
-            background-color: #fff;
-            box-shadow: 0px 0px 19px 5px rgba( 0, 0, 0, 0.19 );
+    <link rel="icon" type="image/x-icon" href="./images/favicon.png">
+    
+    <title>Crud - Login</title>
 
-        }
-        .left{
-            background: url("img/crud.jpg") no-repeat center;
-            background-size: cover;
-            height: 100%;
-        }
-        .right{
-            padding: 25px 40px;
-
-        }
-        h2{
-            position: relative;
-            padding: 0 0 10px;
-            margin-bottom: 10px;
-        }
-        h2::after{
-            content: '';
-            position: absolute;
-            left: 50%;
-            bottom: 0;
-            transform: translateX(-50%);
-            height: 4px;
-            width: 50px;
-            border-radius: 2px;
-         
-            background-color:#9A3C0D ;
-   
-        }
-        .field{
-            width: 100%;
-            border: 2px solid rgba(0, 0, 0, 0.184);
-            outline:none;
-            background-color: #F9A67D;
-            padding: 0.5rem 1rem;
-            font-size: 1.1rem;
-            margin-bottom: 22px;
-            transition: all 0.3s ease-in-out;
-        }
-        .field:hover{
-            background-color: rgba(0,0,0,0.1);
-
-        }
-        textarea{
-            min-height: 150px;
-        }
-        .btn{
-            width: 100%;
-            padding: 0.5rem 1rem;
-            background-color:#9A3C0D;
-            color: #fff;
-            font-size: 1.1rem;
-            border: none;
-            outline:none;
-            cursor: pointer;
-            transition: all 0.3s ease-in-out;
-        }
-        .btn:hover{
-            background-color: #F9A67D;
-        }
-        .field:focus{
-            border: 2px solid #9A3C0D;
-            background-color: #fff;
-        }
-
-        @media screen and (max-width: 880px){
-         
-          .container{
-           
-            padding: 20px;  
-          }
-         
-            .contact-box{
-                grid-template-columns: 1fr;
-            }
-            .left{
-                background-size: cover;
-                height: 100%;
-            }
-            .right{
-                padding: 25px 20px;
-            }
-        }
-            
-       
-   </style>
-     
     
 </head>
 <body>
-    <div class="container">
+    <div class="bg-light container-fluid w-100 d-flex justify-content-center align-items-center" style="height: 100vh;">
         <div class="contact-box">
-            <div class="left"></div>
-            <div class="right">
                 <h2>Crud PortFolio</h2>
-                <form action="login.php" method="post">
-                    <input type="text" name="user" id="user" class="field" placeholder="Usuario" required>
-                    <input type="password" name="pass" id="pass" class="field" placeholder="Password" required>
-                   
-                    <input type="submit" value="Enviar" class="btn">
-                  
-                </form>
+                    <form action="login.php" method="post">
+                        <div class="mb-3">
+                            <label for="user" class="form-label">Usuario</label>
+                            <input type="text" name="user" id="user" class="form-control" placeholder="Usuario" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="pass" class="form-label">Password</label>
+                            <input type="password" name="pass" id="pass" class="form-control" placeholder="Password" required>
+                        </div>
+                        <div class="text-center">
+                            <input class="btn btn-primary" type="submit" value="Ingresar" >
+                        </div>
+                    </form>
         </div>
     </div>
+
     
 </body>
 </html>
